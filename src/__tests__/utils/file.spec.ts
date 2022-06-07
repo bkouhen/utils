801335f84beb6a627df5bef0e76f5784d4dc812b
Boubker <b.kouhen@gmail.com>
2022-06-08 01:48:25 +0200

    Added utils file + specs

---
+++ src/__tests__/utils/file.spec.ts
@@ -0,0 +1,96 @@
+import path from 'path';
+import stream from 'stream';
+import { WinstonLogger } from '../../interfaces/Logger';
+import { countLines, countLinesSync, fileExists, countLinesFromStream } from '../../utils/file';
+
+const assetsPath = path.join(__dirname, '..', '/assets');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+beforeAll(() => {});
+
+afterAll(() => {});
+
+const logger = { debug: jest.fn(), error: jest.fn(), warn: jest.fn() } as unknown as WinstonLogger;
+
+describe('fileExists tests', () => {
+  test('if fileExists function does work correctly', () => {
+    const fileFound = fileExists(`${assetsPath}/sample.json`, false);
+    expect(fileFound).toStrictEqual(true);
+    const fileNotFound = fileExists(`${assetsPath}/sample.not_found.json`, false, logger);
+    expect(fileNotFound).toStrictEqual(false);
+    expect(logger.warn).toBeCalledTimes(1);
+  });
+
+  test('if fileExists function throws when no file found', () => {
+    expect(() => fileExists(`${assetsPath}/sample.not_found.json`, true, logger)).toThrowError();
+  });
+});
+
+describe('countLinesSync tests', () => {
+  test('if it returns the correct number of lines', () => {
+    const lines = countLinesSync(`${assetsPath}/sample.json`, true, logger);
+    expect(lines).toStrictEqual(21);
+  });
+
+  test('if it throws when no file found', () => {
+    expect(() => countLinesSync(`${assetsPath}/sample.not_found.json`)).toThrowError();
+    expect(() => countLinesSync(`${assetsPath}/sample.not_found.json`, true)).toThrowError();
+    expect(() => countLinesSync(`${assetsPath}/sample.not_found.json`, true, logger)).toThrowError();
+  });
+
+  test('if it returns -1 when no file found - no throwing', () => {
+    expect(countLinesSync('', false)).toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(logger.error).toBeCalledTimes(0);
+
+    expect(countLinesSync(`${assetsPath}/sample.not_found.json`, false, undefined)).toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(logger.error).toBeCalledTimes(0);
+
+    expect(countLinesSync(`${assetsPath}/sample.not_found.json`, false, logger)).toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(1);
+    expect(logger.error).toBeCalledTimes(1);
+  });
+});
+
+describe('countLines tests', () => {
+  test('if it returns the correct number of lines', async () => {
+    const lines = await countLines(`${assetsPath}/sample.json`, true, logger);
+    expect(lines).toStrictEqual(21);
+  });
+
+  test('if it throws when no file found', async () => {
+    await expect(() => countLines(`${assetsPath}/sample.not_found.json`)).rejects.toThrowError();
+    await expect(() => countLines(`${assetsPath}/sample.not_found.json`, true)).rejects.toThrowError();
+    await expect(() => countLines(`${assetsPath}/sample.not_found.json`, true, logger)).rejects.toThrowError();
+  });
+
+  test('if it returns -1 when no file found - no throwing', async () => {
+    await expect(countLines('', false)).resolves.toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(logger.error).toBeCalledTimes(0);
+
+    await expect(countLines(`${assetsPath}/sample.not_found.json`, false, undefined)).resolves.toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(logger.error).toBeCalledTimes(0);
+
+    await expect(countLines(`${assetsPath}/sample.not_found.json`, false, logger)).resolves.toStrictEqual(-1);
+    expect(logger.warn).toBeCalledTimes(1);
+    expect(logger.error).toBeCalledTimes(1);
+  });
+});
+
+describe('countLinesFromStream tests', () => {
+  test('if stream returns the correct number of lines', async () => {
+    const readStream = new stream.Readable();
+    readStream.push('This is Line1\n');
+    readStream.push('This is Line2\n');
+    readStream.push('This is Line3\n');
+    readStream.push(null);
+    const lines = await countLinesFromStream(readStream);
+    expect(lines).toStrictEqual(3);
+  });
+});
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,8 +1,32 @@
 import fs from 'fs-extra';
+import stream, { TransformCallback } from 'stream';
 import { execSync } from 'child_process';
+import { pExec } from './process';
 import { WinstonLogger } from '../interfaces/Logger';
+import { lineSplitStream } from '../helpers/file/streamHelper/streamHelper';
 
 const ERR_VALUE = -1;
+
+/**
+ * Function that checks if a file does exist at the mentioned path
+ *
+ * @param filePath Absolute path to the file which should be processed
+ * @param throwError Whether the function should throw if an error happened
+ * @param logger Optional winston logger to be provided to the function
+ * @returns true or false
+ */
+export const fileExists = (filePath: string, throwError: boolean, logger?: WinstonLogger): boolean => {
+  const fileExists = fs.pathExistsSync(filePath);
+  if (!fileExists) {
+    if (throwError) {
+      throw new Error(`countLines -- no file found at ${filePath}`);
+    }
+
+    logger?.warn(`countLines -- no file found at ${filePath}`);
+    return false;
+  }
+  return fileExists;
+};
 
 /**
  * Function that synchronously counts the number of lines of a file
@@ -14,11 +38,8 @@ const ERR_VALUE = -1;
  * there was an error with the wc execution (maybe not in a UNIX environment)
  */
 export const countLinesSync = (filePath: string, throwError: boolean = true, logger?: WinstonLogger): number => {
-  const fileExists = fs.pathExistsSync(filePath);
-  if (!fileExists) {
-    if (throwError) throw new Error(`countLines -- no file found at ${filePath}`);
-
-    logger?.warn(`countLines -- no file found at ${filePath}`);
+  if (!fileExists(filePath, throwError, logger)) {
+    logger?.error(`No file found at ${filePath} - returning -1 as a value`);
     return ERR_VALUE;
   }
 
@@ -29,4 +50,55 @@ export const countLinesSync = (filePath: string, throwError: boolean = true, log
     if (throwError) throw e;
     return ERR_VALUE;
   }
+};
+
+/**
+ * Function that asynchronously counts the number of lines of a file
+ *
+ * @param filePath Absolute path to the file which should be processed
+ * @param throwError Whether the function should throw if an error happened
+ * @param logger Optional winston logger to be provided to the function
+ * @returns an integer that should be positive. If result is -1, it means that either the file has not been found or
+ * there was an error with the wc execution (maybe not in a UNIX environment)
+ */
+export const countLines = async (
+  filePath: string,
+  throwError: boolean = true,
+  logger?: WinstonLogger,
+): Promise<number> => {
+  if (!fileExists(filePath, throwError, logger)) {
+    logger?.error(`No file found at ${filePath} - returning -1 as a value`);
+    return ERR_VALUE;
+  }
+
+  try {
+    const numberOfLines = (await pExec(`wc -l ${filePath}`)).stdout;
+    return parseInt(numberOfLines);
+  } catch (e) {
+    logger?.error(e);
+    if (throwError) throw e;
+    return ERR_VALUE;
+  }
+};
+
+/**
+ * Function that asynchronously counts the number of lines from a readable stream as an input
+ *
+ * @param readableStream Input readable stream
+ * @returns the number of lines of the file read through the input stream
+ */
+export const countLinesFromStream = async (readableStream: stream.Readable): Promise<number> => {
+  let lines = 0;
+  return new Promise((resolve) => {
+    const lineSplitter = lineSplitStream();
+    const countStream = new stream.Transform({
+      transform: (chunk: string, encoding: BufferEncoding, next: TransformCallback) => {
+        lines++;
+        next(null, chunk);
+      },
+    });
+    const noop = fs.createWriteStream('/dev/null');
+    noop.on('finish', () => resolve(lines));
+    readableStream.pipe(lineSplitter).pipe(countStream).pipe(noop);
+  });
 };
